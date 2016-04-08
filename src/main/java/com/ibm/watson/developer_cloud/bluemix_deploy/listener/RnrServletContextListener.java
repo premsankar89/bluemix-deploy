5c01e4a655d2cae520f7ef88370032a2fc81cfdb
David Terry <dgterry@us.ibm.com>
2016-04-08 16:09:09 -0400

    add ServletContextListener

---
+++ src/main/java/com/ibm/watson/developer_cloud/bluemix_deploy/listener/RnrServletContextListener.java
@@ -0,0 +1,16 @@
+package com.ibm.watson.developer_cloud.bluemix_deploy.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class RnrServletContextListener implements ServletContextListener {
+   
+	public void contextDestroyed(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+	}
+
+	public void contextInitialized(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+}
