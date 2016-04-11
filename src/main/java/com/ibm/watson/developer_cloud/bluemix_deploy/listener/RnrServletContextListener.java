389e174d40d3a307b0253bbe043222dfe6dccdbb
David Terry <dgterry@us.ibm.com>
2016-04-11 11:41:17 -0400

    add resources

--- src/main/java/com/ibm/watson/developer_cloud/bluemix_deploy/listener/RnrServletContextListener.java
+++ src/main/java/com/ibm/watson/developer_cloud/bluemix_deploy/listener/RnrServletContextListener.java
@@ -3,6 +3,11 @@ package com.ibm.watson.developer_cloud.bluemix_deploy.listener;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
+import com.ibm.watson.developer_cloud.retrieve_and_rank.v1.RetrieveAndRank;
+import com.ibm.watson.developer_cloud.retrieve_and_rank.v1.model.SolrCluster;
+import com.ibm.watson.developer_cloud.retrieve_and_rank.v1.model.SolrCluster.Status;
+import com.ibm.watson.developer_cloud.retrieve_and_rank.v1.model.SolrClusterOptions;
+
 public class RnrServletContextListener implements ServletContextListener {
    
 	public void contextDestroyed(ServletContextEvent arg0) {
@@ -10,6 +15,34 @@ public class RnrServletContextListener implements ServletContextListener {
 	}
 
 	public void contextInitialized(ServletContextEvent arg0) {
-		System.out.println("testing that this worked");
+		// TODO refactor into helper class or at least into methods
+		RetrieveAndRank service = new RetrieveAndRank();
+		
+		createCluster(service);
+		
+		
+	}
+
+	private void createCluster(RetrieveAndRank service) {
+		// 1 create the Solr Cluster
+		// TODO place in easier to manipulate place? how large a cluster?
+		SolrClusterOptions options = new SolrClusterOptions("my-cluster-name", 1);
+		SolrCluster cluster = service.createSolrCluster(options);
+		System.out.println("Solr cluster: " + cluster);
+		
+		// 2 wait until the Solr Cluster is available
+		while (cluster.getStatus() == Status.NOT_AVAILABLE) {
+		  try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} // sleep 10 seconds
+		  cluster = service.getSolrCluster(cluster.getId());
+		  System.out.println("Solr cluster status: " + cluster.getStatus());
+		}
+		
+		// 3 list Solr Clusters
+		System.out.println("Solr clusters: " + service.getSolrClusters());
 	}
 }
