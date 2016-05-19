b59c8d8480a6266cf6c0e740c92436a624d61130
David Terry <dgterry@us.ibm.com>
2016-05-19 12:10:22 -0400

    enhance deploy story

---
+++ src/main/java/com/ibm/watson/developer_cloud/bluemix_deploy/listener/RnRConstants.java
@@ -0,0 +1,12 @@
+package com.ibm.watson.developer_cloud.bluemix_deploy.listener;
+
+public class RnRConstants {
+	
+	// Schema Fields
+	public static final String SCHEMA_FIELD_BODY = "body";
+	public static final String SCHEMA_FIELD_CONTENT_HTML = "contentHtml";
+	public static final String SCHEMA_FIELD_CONTENT_TEXT = "contentText";
+	public static final String SCHEMA_FIELD_ID = "id";
+	public static final String SCHEMA_FIELD_SOURCE_URL = "sourceUrl";
+	public static final String SCHEMA_FIELD_TITLE = "title";
+}
--- src/main/java/com/ibm/watson/developer_cloud/bluemix_deploy/setup/RnrSetupThread.java
+++ src/main/java/com/ibm/watson/developer_cloud/bluemix_deploy/setup/RnrSetupThread.java
@@ -2,7 +2,32 @@ package com.ibm.watson.developer_cloud.bluemix_deploy.setup;
 
 import java.io.File;
 import java.io.IOException;
-
+import java.io.FileReader;
+import java.lang.Exception;
+import java.net.URL;
+import java.net.URI;
+import java.util.Collection;
+import java.util.ArrayList;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonParser;
+import org.apache.http.HttpException;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpRequestInterceptor;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.AuthState;
+import org.apache.http.auth.Credentials;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.impl.auth.BasicScheme;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.protocol.HttpContext;
+import org.apache.http.protocol.HttpCoreContext;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
 import org.apache.solr.client.solrj.request.CollectionAdminRequest;
@@ -10,45 +35,140 @@ import org.apache.solr.client.solrj.response.CollectionAdminResponse;
 import org.apache.solr.client.solrj.response.UpdateResponse;
 import org.apache.solr.common.SolrInputDocument;
 
+import com.ibm.watson.developer_cloud.bluemix_deploy.listener.RnRConstants;
 import com.ibm.watson.developer_cloud.retrieve_and_rank.v1.RetrieveAndRank;
 import com.ibm.watson.developer_cloud.retrieve_and_rank.v1.model.SolrCluster;
 import com.ibm.watson.developer_cloud.retrieve_and_rank.v1.model.SolrClusterOptions;
 import com.ibm.watson.developer_cloud.retrieve_and_rank.v1.model.SolrCluster.Status;
+import com.ibm.watson.developer_cloud.retrieve_and_rank.v1.model.SolrClusterList;
 
 public class RnrSetupThread extends Thread {
 	public void run () {
-		// TODO need way to not do this if done before
-		
 				// TODO refactor into helper class or at least into methods
 				System.out.println("got to 1");
 				RetrieveAndRank service = new RetrieveAndRank();
+				
+				if (isAlreadySetup(service)) {
+					return;
+				}
+				
 				System.out.println("got to 2");
 				SolrCluster cluster = createCluster(service);
-				System.out.println("got to 5");
+				System.out.println("got to 3");
 				
 				uploadConfiguration(service, cluster);
-				
-				HttpSolrClient solrClient = new HttpSolrClient(service.getSolrUrl(cluster.getId()));
-				
-				createCollection(service, cluster, solrClient);
-				
+				System.out.println("got to 4");
+				JsonObject vcap = new JsonParser().parse(System.getenv("VCAP_SERVICES")).getAsJsonObject();
+                                JsonObject rr = vcap.getAsJsonArray("retrieve_and_rank").get(0).getAsJsonObject();
+                                JsonObject credentials = rr.getAsJsonObject("credentials");
+             
+                                String username = credentials.get("username").getAsString();
+                                String password = credentials.get("password").getAsString();
+                                HttpSolrClient solrClient = getSolrClient(service.getSolrUrl(cluster.getId()), username, password);
+				try{
+				createCollection1(solrClient);
+				System.out.println("got to 5");
 				indexDocuments(solrClient);
+					
+				}catch(Exception e){
+				System.out.println(e);	
+				}
 	}
-	
-	private void indexDocuments(HttpSolrClient solrClient) {
-		SolrInputDocument document = new SolrInputDocument();
-		document.addField("body", "experimental investigation of the aerodynamics of a wing in a slipstream .   an experimental study of a wing in a propeller slipstream was made in order to determine the spanwise distribution of the lift increase due to slipstream at different angles of attack of the wing and at different free stream to slipstream velocity ratios .  the results were intended in part as an evaluation basis for different theoretical treatments of this problem .   the comparative span loading curves, together with supporting evidence, showed that a substantial part of the lift increment produced by the slipstream was due to a /destalling/ or boundary-layer-control effect .  the integrated remaining lift increment, after subtracting this destalling lift, was found to agree well with a potential flow theory .   an empirical evaluation of the destalling effects was made for the specific configuration of the experiment .");
-		document.addField("title", "experimental investigation of the aerodynamics of a wing in a slipstream .");
 
+	/**
+	 * Makes a call to get the number of clusters, if it is > 0
+	 * then we assume the setup has already been done and we skip it
+	 * 
+	 * @param service
+	 * @return
+	 */
+	private boolean isAlreadySetup(RetrieveAndRank service) {
+		SolrClusterList clusters = service.getSolrClusters();
+
+		return clusters.getSolrClusters().size() > 0 ? true : false;
+	}
+
+private static HttpSolrClient getSolrClient(String uri, String username, String password) {
+    return new HttpSolrClient(uri, createHttpClient(uri, username, password));
+  }
+
+  private static HttpClient createHttpClient(String uri, String username, String password) {
+    final URI scopeUri = URI.create(uri);
+
+    final BasicCredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+    credentialsProvider.setCredentials(new AuthScope(scopeUri.getHost(), scopeUri.getPort()),
+        new UsernamePasswordCredentials(username, password));
+
+    final HttpClientBuilder builder = HttpClientBuilder.create()
+        .setMaxConnTotal(128)
+        .setMaxConnPerRoute(32)
+        .setDefaultRequestConfig(RequestConfig.copy(RequestConfig.DEFAULT).setRedirectsEnabled(true).build())
+        .setDefaultCredentialsProvider(credentialsProvider)
+        .addInterceptorFirst(new PreemptiveAuthInterceptor());
+    return builder.build();
+  }
+  
+   private static class PreemptiveAuthInterceptor implements HttpRequestInterceptor {
+    public void process(final HttpRequest request, final HttpContext context) throws HttpException {
+      final AuthState authState = (AuthState) context.getAttribute(HttpClientContext.TARGET_AUTH_STATE);
+
+      if (authState.getAuthScheme() == null) {
+        final CredentialsProvider credsProvider = (CredentialsProvider) context
+            .getAttribute(HttpClientContext.CREDS_PROVIDER);
+        final HttpHost targetHost = (HttpHost) context.getAttribute(HttpCoreContext.HTTP_TARGET_HOST);
+        final Credentials creds = credsProvider.getCredentials(new AuthScope(targetHost.getHostName(),
+            targetHost.getPort()));
+        if (creds == null) {
+          throw new HttpException("No creds provided for preemptive auth.");
+        }
+        authState.update(new BasicScheme(), creds);
+      }
+    }
+  }
+  
+	private void indexDocuments(HttpSolrClient solrClient) {
+		URL url = this.getClass().getClassLoader().getResource("file4.json");
+		File dataFile = null;
+		try {
+                    dataFile = new File(url.toURI());
+                } catch(Exception e) {
+                    System.out.println(dataFile.getPath());
+               }
+            
+ JsonArray a =null;
+ try{
+ 	a = (JsonArray)new JsonParser().parse(new FileReader(dataFile)).getAsJsonArray();
+ }
+ catch(Exception e){
+ 	System.out.println(e);
+ }
+ Collection<SolrInputDocument> docs = new ArrayList<SolrInputDocument>();
+ for (int i = 0, size = a.size(); i < size; i++)
+    {
+  	SolrInputDocument document = new SolrInputDocument();
+    JsonObject car = a.get(i).getAsJsonObject();
+
+    int id = car.get(RnRConstants.SCHEMA_FIELD_ID).getAsInt();
+    String title = (String) car.get(RnRConstants.SCHEMA_FIELD_TITLE).getAsString();
+    String body = (String) car.get(RnRConstants.SCHEMA_FIELD_BODY).getAsString();
+    String sourceUrl = (String) car.get(RnRConstants.SCHEMA_FIELD_SOURCE_URL).getAsString();
+    document.addField(RnRConstants.SCHEMA_FIELD_ID, id);
+    document.addField(RnRConstants.SCHEMA_FIELD_TITLE, title);
+    document.addField(RnRConstants.SCHEMA_FIELD_BODY, body);
+    document.addField(RnRConstants.SCHEMA_FIELD_SOURCE_URL,sourceUrl);
+    // TODO need to ingest html into contentHtml
+    docs.add(document);
+  }
 		System.out.println("Indexing document...");
+
 		UpdateResponse addResponse;
 		try {
-			addResponse = solrClient.add("example_collection", document);
+			addResponse = solrClient.add("car_collection", docs);
 			
 			System.out.println(addResponse);
 
 			// Commit the document to the index so that it will be available for searching.
-			solrClient.commit("example_collection");
+			solrClient.commit("car_collection");
 			System.out.println("Indexed and committed document.");
 		} catch (SolrServerException e) {
 			// TODO Auto-generated catch block
@@ -59,46 +179,43 @@ public class RnrSetupThread extends Thread {
 		}
 	}
 
-	private void createCollection(RetrieveAndRank service, SolrCluster cluster, HttpSolrClient solrClient) {
-		CollectionAdminRequest.Create createCollectionRequest =
-		        new CollectionAdminRequest.Create();
-		createCollectionRequest.setCollectionName("example_collection");
-		createCollectionRequest.setConfigName("example_config");
+private static void createCollection1(HttpSolrClient solrClient) throws SolrServerException, IOException {
+    final CollectionAdminRequest.Create createCollectionRequest =
+        new CollectionAdminRequest.Create();
+    createCollectionRequest.setCollectionName("car_collection");
+    createCollectionRequest.setConfigName("car_config");
 
-		System.out.println("Creating collection...");
-		
-		CollectionAdminResponse response;
-		try {
-			response = createCollectionRequest.process(solrClient);
-			
-			  if (!response.isSuccess()) {
-			      System.out.println(response.getErrorMessages());
-			      throw new IllegalStateException("Failed to create collection: "
-			          + response.getErrorMessages().toString());
-			    }
-			System.out.println("Collection created.");
-			System.out.println(response);
-		} catch (SolrServerException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+    System.out.println("Creating collection...");
+    final CollectionAdminResponse response = createCollectionRequest.process(solrClient);
+    if (!response.isSuccess()) {
+      System.out.println(response.getErrorMessages());
+      throw new IllegalStateException("Failed to create collection: "
+          + response.getErrorMessages().toString());
+    }
+    System.out.println("Collection created.");
+  }
+
 
 	private void uploadConfiguration(RetrieveAndRank service, SolrCluster cluster) {
-		File configZip = new File("/resources/cranfield_solr_config.zip");
+		URL url = this.getClass().getClassLoader().getResource("solr_config.zip");
+		File configZip = null;
+		try {
+                    configZip = new File(url.toURI());
+                } catch(Exception e) {
+                    System.out.println(configZip.getPath());
+               }
+		
 		
 		// TODO extract name? error handling, check for 200
 		service.uploadSolrClusterConfigurationZip(cluster.getId(),
-		"example_config", configZip);
+		"car_config", configZip);
+			System.out.println("Uploaded configuration.");
 	}
 
 	private SolrCluster createCluster(RetrieveAndRank service) {
 		// 1 create the Solr Cluster
 		// TODO place in easier to manipulate place? how large a cluster?
-		SolrClusterOptions options = new SolrClusterOptions("my-cluster-name", 1);
+		SolrClusterOptions options = new SolrClusterOptions("car_cluster", 1);
 		System.out.println("got to 3");
 		SolrCluster cluster = service.createSolrCluster(options);
 		System.out.println("got to 4");
